8cb3ac6af6b6879efed2751ed9755c5d5027b701
wangxiaosen <wangxiaosen@9fbank.com.cn>
2020-11-25 17:46:08 +0800

    公共代码提取

--- static/js/home.js
+++ static/js/home.js
@@ -1,2 +1,87 @@
 $('#nav').load('common_nav.html');
 $('#footer').load('common_footer.html');
+
+$(function () {
+    $('#loading').css('display', 'block');
+    loadClothes();
+    loadCosmetics();
+    loadHotProduct();
+    loadNewestProduct();
+    $('#loading').css('display', 'none');
+});
+
+// 加载服饰推荐
+function loadClothes() {
+    $.ajax({
+        type: 'post',
+        url: 'http://localhost:9527/user/login',
+        data: $('#dataForm').serialize(),
+        xhrFields: {
+            withCredentials: true
+        },
+        success: function f(res) {
+            if (res.code === 2000) {
+
+            } else {
+
+            }
+        }
+    });
+}
+
+// 加载美妆推荐
+function loadCosmetics() {
+    $.ajax({
+        type: 'post',
+        url: 'http://localhost:9527/user/login',
+        data: $('#dataForm').serialize(),
+        xhrFields: {
+            withCredentials: true
+        },
+        success: function f(res) {
+            if (res.code === 2000) {
+
+            } else {
+
+            }
+        }
+    });
+}
+
+// 加载热门商品
+function loadHotProduct() {
+    $.ajax({
+        type: 'post',
+        url: 'http://localhost:9527/user/login',
+        data: $('#dataForm').serialize(),
+        xhrFields: {
+            withCredentials: true
+        },
+        success: function f(res) {
+            if (res.code === 2000) {
+
+            } else {
+
+            }
+        }
+    });
+}
+
+// 加载最新上架
+function loadNewestProduct() {
+    $.ajax({
+        type: 'post',
+        url: 'http://localhost:9527/user/login',
+        data: $('#dataForm').serialize(),
+        xhrFields: {
+            withCredentials: true
+        },
+        success: function f(res) {
+            if (res.code === 2000) {
+
+            } else {
+
+            }
+        }
+    });
+}
